93f3e4cb707ac93bceb0e37966c54cbeb3783c93
ChalotohrnSricharoen <105422277+ChalotohrnSricharoen@users.noreply.github.com>
2022-08-18 16:14:25 +0700

    products

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,6 +13,20 @@ app.use(express.static(path.join(__dirname,"/public/")));
 
 app.set("views","./src/views");
 app.set("view engine","ejs");
+
+productRouter.route("/").get((req,res) =>{
+    res.render("products" , {
+        products : [
+        {productTitle: 'iphone 11' , productDescription : '128Gb' , productPrice : 30000},
+        {productTitle: 'iphone 12' , productDescription : '128Gb' , productPrice : 40000},
+        {productTitle: 'iphone 13' , productDescription : '128Gb' , productPrice : 50000},
+    ],
+ });
+})
+productRouter.route("/1").get((req,res) =>{
+    res.send("Hello 1");
+})
+app.use("/products",productRouter)
 
 app.get("/",(req,res) => {
     res.render('index',{username: "Chalotohrn" , customers : ["Chalothorn","John","Demon"]});
